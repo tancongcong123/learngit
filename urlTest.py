cd7e55276a08d5a940e58f188a0c3185af1608b7
tancongcong123 <tancongcong_123@163.com>
2018-05-10 16:37:09 +0800

    commit test

---
+++ urlTest.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+from urllib import request,parse
+
+with request.urlopen('https://api.douban.com/v2/book/2129650') as f:
+	data = f.read()
+	print('Status:',f.status,f.reason)
+	for k,v in f.getheaders():
+		print('%s:%s' % (k,v)) 
+	print('Data:',data.decode('utf-8'))
