7889ee923202f63d8a57d737c1e1dcf59f86de5e
tancongcong123 <tancongcong_123@163.com>
2018-08-30 15:29:31 +0800

    webview

--- urlTest.py
+++ urlTest.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 from urllib import request,parse
 
-with request.urlopen('https://api.douban.com/v2/book/2129650') as f:
+# url='https://api.douban.com/v2/book/2129650'
+url = 'https://www.ebay.com/itm/adidas-NMD-R2-Primeknit-Shoes-Mens/153057156614?_trkparms=5373%3A5000006436%7C5374%3AFashion%7C5079%3A5000006436'
+
+with request.urlopen(url) as f:
 	data = f.read()
 	print('Status:',f.status,f.reason)
 	for k,v in f.getheaders():
